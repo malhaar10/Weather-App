854007b004482aab4ac3994ac97fd67e4c3e3da7
malhaar10 <123005720+malhaar10@users.noreply.github.com>
2025-04-14 16:30:25 +0530

    style: Enhance UI of login and signup pages with improved design and error handling

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'auth.dart'; // Import the AuthService
+import 'auth.dart';
 import 'home_city.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -21,64 +21,88 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                String email = _emailController.text.trim();
-                String password = _passwordController.text.trim();
+      appBar: AppBar(
+        title:
+            const Text('Login', style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: const Color.fromARGB(255, 0, 100, 181),
+      ),
+      body: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                    labelText: 'Email',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder()),
+              ),
+              const Divider(color: Colors.transparent),
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                    labelText: 'Password',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder()),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  String email = _emailController.text.trim();
+                  String password = _passwordController.text.trim();
 
-                // Attempt to log in
-                User? user = await _authService.logIn(email, password);
-                if (user != null) {
-                  // Save login state
-                  SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                  await prefs.setBool('isLoggedIn', true);
+                  // Attempt to log in
+                  User? user = await _authService.logIn(email, password);
+                  if (user != null) {
+                    // Save login state
+                    SharedPreferences prefs =
+                        await SharedPreferences.getInstance();
+                    await prefs.setBool('isLoggedIn', true);
 
-                  // Navigate to HomeCity
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const HomeCity()),
-                  );
-                } else {
-                  setState(() {
-                    _errorMessage =
-                        'Login failed. Please check your credentials.';
-                  });
-                }
-              },
-              child: const Text('Login'),
-            ),
-            if (_errorMessage.isNotEmpty)
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text(
-                  _errorMessage,
-                  style: const TextStyle(color: Colors.red),
+                    // Navigate to HomeCity
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomeCity()),
+                    );
+                  } else {
+                    setState(() {
+                      _errorMessage =
+                          'Login failed. Please check your credentials.';
+                    });
+                  }
+                },
+                child: const Text('Login',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold)),
+              ),
+              if (_errorMessage.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(
+                    _errorMessage,
+                    style: const TextStyle(fontSize: 15, color: Colors.red),
+                  ),
+                ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(
+                      context, '/signup'); // Navigate to signup page
+                },
+                child: const Text(
+                  'Don\'t have an account? Sign up',
+                  style: TextStyle(color: Colors.white, fontSize: 15),
                 ),
               ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(
-                    context, '/signup'); // Navigate to signup page
-              },
-              child: const Text('Don\'t have an account? Sign up'),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/signup.dart
+++ lib/signup.dart
@@ -21,56 +21,76 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Sign Up')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                String email = _emailController.text.trim();
-                String password = _passwordController.text.trim();
-
-                // Attempt to sign up
-                User? user = await _authService.signUp(email, password);
-                if (user != null) {
-                  // Save login state
-                  SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                  await prefs.setBool('isLoggedIn', true);
-
-                  // Navigate to HomeCity
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const HomeCity()),
-                  );
-                } else {
-                  setState(() {
-                    _errorMessage = 'Signup failed. Please try again.';
-                  });
-                }
-              },
-              child: const Text('Sign Up'),
-            ),
-            if (_errorMessage.isNotEmpty)
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text(
-                  _errorMessage,
-                  style: const TextStyle(color: Colors.red),
-                ),
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+        backgroundColor: const Color.fromARGB(255, 0, 100, 181),
+      ),
+      body: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                    labelText: 'Email',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder()),
               ),
-          ],
+              const Divider(color: Colors.transparent),
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                    labelText: 'Password',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder()),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  String email = _emailController.text.trim();
+                  String password = _passwordController.text.trim();
+
+                  // Attempt to sign up
+                  User? user = await _authService.signUp(email, password);
+                  if (user != null) {
+                    // Save login state
+                    SharedPreferences prefs =
+                        await SharedPreferences.getInstance();
+                    await prefs.setBool('isLoggedIn', true);
+
+                    // Navigate to HomeCity
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomeCity()),
+                    );
+                  } else {
+                    setState(() {
+                      _errorMessage = 'Signup failed. Please try again.';
+                    });
+                  }
+                },
+                child: const Text('Sign Up',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold)),
+              ),
+              if (_errorMessage.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(
+                    _errorMessage,
+                    style: const TextStyle(fontSize: 15, color: Colors.red),
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
